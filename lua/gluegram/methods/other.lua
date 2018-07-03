fc6f043b9489b4c4ac9e171b2cbcb17d3cdf643d
Mad Kernel <amd@default.im>
2018-07-03 16:29:40 +0300

    + BOT_MT:ChatAction(chat_id, action)

--- lua/gluegram/methods/other.lua
+++ lua/gluegram/methods/other.lua
@@ -1,8 +1,26 @@
--- TLG_ACT_TYPING   = "typing"
--- TLG_ACT_PHOTO    = "upload_photo"
--- TLG_ACT_VIDEO    = "record_video"
--- TLG_ACT_AUDIO    = "record_audio"
--- TLG_ACT_DOCUMENT = "upload_document"
--- TLG_ACT_LOCATION = "find_location"
+local ACTION = TLG.NewMethod("sendChatAction")
 
--- TLG.RegisterMethod("sendChatAction",{})
+function ACTION:SendTo(chat_id)
+	return self:SetParam("chat_id", chat_id)
+end
+
+function ACTION:Set(action)
+	return self:SetParam("action", action)
+end
+
+
+
+
+TLG.ACT_TYPING   = "typing"
+TLG.ACT_PHOTO    = "upload_photo"
+TLG.ACT_VIDEO    = "record_video"
+TLG.ACT_AUDIO    = "record_audio"
+TLG.ACT_DOCUMENT = "upload_document"
+TLG.ACT_LOCATION = "find_location"
+
+local BOT_MT = TLG.GetObject("BOT")
+function BOT_MT:ChatAction(chat_id, action)
+	return self:Request(ACTION):Set(action):SendTo(chat_id)
+end
+
+-- TLG_CORE_BOT:ChatAction(TLG_AMD, TLG.ACT_PHOTO):Send(prt)
