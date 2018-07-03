637cbd55f46a45927a7ed74247b4fe4a6c874849
Mad Kernel <amd@default.im>
2018-07-03 16:28:35 +0300

    + TLG.InputMedia()
    Creates something like album of photos and videos using `BOT_MT:MediaGroup(chat_id, tMedia)`

---
+++ lua/gluegram/methods/objects/inputmedia.lua
@@ -0,0 +1,32 @@
+-- https://core.telegram.org/bots/api#inputmedia
+-- Пример в sendmediagroup.lua
+
+local MEDIA_MT = {}
+MEDIA_MT.__index = MEDIA_MT
+
+function MEDIA_MT:AddPhoto(photo, caption, parse_mode)
+	table.insert(self,{
+		type = "photo",
+		media = photo, -- url or tlg id
+		caption = caption, -- 0-200 chars
+		parse_mode = parse_mode, -- html, markdown
+	})
+end
+
+function MEDIA_MT:AddVideo(photo, caption, parse_mode)
+	table.insert(self,{
+		type = "video",
+		media = photo,
+		caption = caption,
+		parse_mode = parse_mode,
+
+		-- width,height,duration,supports_streaming
+	})
+end
+
+
+
+
+function TLG.InputMedia() -- 2-10 items
+	return setmetatable({},MEDIA_MT)
+end
